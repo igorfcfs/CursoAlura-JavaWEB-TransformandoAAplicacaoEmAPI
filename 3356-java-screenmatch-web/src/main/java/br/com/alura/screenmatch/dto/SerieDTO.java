877a4e99a9bc1ed0b79443790dbdb51d00dc701e
igorfcfs <igorfcfs@gmail.com>
2024-02-12 13:01:29 -0300

    Fornecendo Dados para o Front

---
+++ 3356-java-screenmatch-web/src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,9 @@
+package br.com.alura.screenmatch.dto;
+
+import br.com.alura.screenmatch.model.Categoria;
+
+//Data Transfer Object - DTO
+public record SerieDTO(Long id, String titulo, Integer totalTemporadas, Double avaliacao, Categoria genero,
+		String atores, String poster, String sinopse) {
+
+}
